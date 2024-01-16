19da736dcc808d663b550d7fb2eb199c963c77db
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2024-01-16 15:53:27 +0700

    feat: add code optional requirement

--- app/questions.js
+++ app/questions.js
@@ -4,11 +4,25 @@ import { ObjectId } from "mongodb";
 
 const questionRouter = Router();
 
-const collection = db.collection("questions");
+const questionsDB = db.collection("questions");
+
+const answerDB = db.collection("answer");
 
 questionRouter.get("/", async (req, res) => {
   try {
-    const resData = await collection.find().toArray();
+    const name = req.query.keywords;
+    const category = req.query.category;
+    const query = {};
+
+    if (name) {
+      query.title = new RegExp(name, "ig");
+    }
+
+    if (category) {
+      query.category = new RegExp(category, "ig");
+    }
+
+    const resData = await questionsDB.find(query).toArray();
     return res.json(resData);
   } catch {
     return res.json({ message: `Cannot to query question` });
@@ -17,11 +31,26 @@ questionRouter.get("/", async (req, res) => {
 
 questionRouter.get("/:questionId", async (req, res) => {
   try {
-    const resData = await collection.findOne({
+    const resData = await questionsDB.findOne({
       _id: new ObjectId(req.params.questionId),
     });
     return res.json(resData);
-  } catch (error) {
+  } catch {
+    return res.json({
+      message: `Cannot to query question at id ${req.params.questionId}`,
+    });
+  }
+});
+
+questionRouter.get("/:questionId/answer", async (req, res) => {
+  try {
+    const resData = await answerDB
+      .find({
+        questionId: req.params.questionId,
+      })
+      .toArray();
+    return res.json(resData);
+  } catch {
     return res.json({
       message: `Cannot to query question at id ${req.params.questionId}`,
     });
@@ -30,7 +59,7 @@ questionRouter.get("/:questionId", async (req, res) => {
 
 questionRouter.post("/", async (req, res) => {
   try {
-    await collection.insertOne({
+    await questionsDB.insertOne({
       ...req.body,
       opinion: 0,
       created_at: new Date(),
@@ -42,9 +71,28 @@ questionRouter.post("/", async (req, res) => {
   return res.json({ message: "Question has been created successfully" });
 });
 
+questionRouter.post("/:questionId/answer", async (req, res) => {
+  try {
+    if (req.body.content.length >= 300) {
+      return res.json({ message: "Please Input Less Than 300 word" });
+    } else {
+      await answerDB.insertOne({
+        questionId: req.params.questionId,
+        ...req.body,
+        opinion: 0,
+        created_at: new Date(),
+      });
+    }
+  } catch {
+    return res.json({ message: "Cannot to create answer" });
+  }
+
+  return res.json({ message: "Answer has been created successfully" });
+});
+
 questionRouter.put("/:questionId", async (req, res) => {
   try {
-    await collection.updateOne(
+    await questionsDB.updateOne(
       { _id: new ObjectId(req.params.questionId) },
       {
         $set: req.body,
@@ -59,9 +107,29 @@ questionRouter.put("/:questionId", async (req, res) => {
   });
 });
 
+questionRouter.put("/:questionId/answer/:answerId", async (req, res) => {
+  try {
+    await answerDB.updateOne(
+      { _id: new ObjectId(req.params.answerId) },
+      {
+        $set: req.body,
+      }
+    );
+  } catch {
+    return res.json({ message: "Cannot to Update answer" });
+  }
+
+  return res.json({
+    message: "answer has been updated successfully",
+  });
+});
+
 questionRouter.delete("/:questionId", async (req, res) => {
   try {
-    await collection.deleteOne({ _id: new ObjectId(req.params.questionId) });
+    await questionsDB.deleteOne({ _id: new ObjectId(req.params.questionId) });
+    await answerDB.deleteMany({
+      questionId: req.params.questionId,
+    });
   } catch {
     return res.json({ message: "Cannot to Delete question" });
   }
